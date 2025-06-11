22b62684380f2a6d7293e69a5331f6ba64c25c32
RomyMartinez <ngcsmrtnz@gmail.com>
2025-06-10 21:51:12 -0300

    Create column user_id

--- models/meal.py
+++ models/meal.py
@@ -3,6 +3,8 @@ from flask_login import UserMixin
 
 class Meal(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable = False)
     description = db.Column(db.String(180), nullable = False)
     date = db.Column(db.String(180), nullable = False)
     isDiet = db.Column(db.Boolean, nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
